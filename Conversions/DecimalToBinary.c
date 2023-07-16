ad9d2c05e37366c23bdfdc3e69ae37c9b4bc0e05
MehmetGonenc <70261640+MehmetEmirGonenc@users.noreply.github.com>
2023-07-16 13:42:05 +0000

    V0

---
+++ Conversions/DecimalToBinary.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char* argv[])
+{
+    //Check inputs
+    if (argc != 2)
+    {
+        printf("True usage ./DecimalToBinary <number>\n");
+        return 1;
+    }
+    int number = atoi(argv[1]);
+    int x = 0;
+    while (pow(2,x) <= number)
+    {
+        x++;
+    }
+
+    for (int i = x-1; i >= 0;i--)
+    {
+        if (pow(2,i) <= number)
+        {
+            printf("1");
+            number -= pow(2,i);
+        }
+        else
+        {
+            printf("0");
+        }
+    }
+    printf("\n");
+    // Make Command : : gcc -o DecimalToBinary DecimalToBinary.c -lm
+}
