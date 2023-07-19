7d56c1b32e26e7f3919e8aa7ddbd07566a39347f
MehmetGonenc <70261640+MehmetEmirGonenc@users.noreply.github.com>
2023-07-19 12:05:06 +0000

    V0

--- EditingVoices/Editor.c
+++ EditingVoices/Editor.c
@@ -4,14 +4,43 @@
 
 int main(int argc, char* argv[])
 {
+    typedef struct 
+    {
+        unsigned FrameSync :  11;   //Frame sync
+        unsigned ID :  2;
+        unsigned LayerIndex :  2;
+        unsigned ProtectBit :  1;
+        unsigned BitrateIndex :  4;
+        unsigned RateIndex :  2;
+        unsigned PaddingBit :  1;
+        unsigned PrivateBit :  1;
+        unsigned ChannelMode :  2;
+        unsigned ModeExtension :  2;
+        unsigned CopyrightBit :  1;
+        unsigned OriginalBit :  1;
+        unsigned Emphasis :  2;
+    }MP3HEADER;
+    
     FILE *input = fopen(argv[1], "r");
 
-    uint8_t buffer[35];
-    fread(&buffer, sizeof(uint8_t) * 35, 1, input);
+    MP3HEADER buffer_header;
+    fread(&buffer_header, sizeof(MP3HEADER), 1, input);
     
-    for (int i = 0; i < 35; i++)
+    FILE *output = fopen(argv[2], "w");
+    fwrite(&buffer_header, sizeof(MP3HEADER), 1, output);
+    
+    uint8_t buffer;
+    fseek(input, 0, SEEK_END);
+    fseek(input, -7, SEEK_CUR);
+    while (ftell(input) > 32)
     {
-        printf("%i -->%u\n",i, buffer[i]);
+        printf("%li\n",ftell(input));
+        fread(&buffer, sizeof(uint8_t), 1, input);
+        
+        fwrite(&buffer,sizeof(uint8_t), 1, output);
+        fseek(input, -16, SEEK_CUR);
     }
+    
     fclose(input);
+    fclose(output);
 }
